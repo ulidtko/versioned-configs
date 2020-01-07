62b3ca1a32ada8ce336c846b67110fa13cc77101
max ulidtko <ulidtko@gmail.com>
2020-01-07 15:54:30 +0200

    install.sh: wipe shellcheck issues

--- install.sh
+++ install.sh
@@ -4,23 +4,26 @@ MYDIR="$(cd "$(dirname "$0")" && pwd)"
 
 if [ "$TERM" = cygwin ]; then
 	symlink () {
-		local linkname="$2"
-		local linktarget="$1"
+		symlink_name="$2"
+		symlink_target="$1"
 
 		if [ $# -gt 2 ]; then
 			echo >&2 "only two argument form is accepted: ln <LINKTARGET> <LINKNAME>"
 			exit 1
 		fi
 
-		local mklinkflags=""
-		[ -d "$linktarget" ] && mklinkflags="/D"
+		symlink_flags=""
+		[ -d "$symlink_target" ] && symlink_flags="/D"
 
-		[ -e "$linkname" ] && {
-			echo >&2 "link name \"$linkname\" is already a file, skipping"
+		[ -e "$symlink_name" ] && {
+			echo >&2 "link name \"$symlink_name\" is already a file, skipping"
 			return
 		}
 
-		cmd /C mklink $mklinkflags "$(cygpath -w "$linkname")" "$(cygpath -w "$linktarget")"
+		cmd /C mklink \
+			"$symlink_flags" \
+			"$(cygpath -w "$symlink_name")" \
+			"$(cygpath -w "$symlink_target")"
 	}
 
 else
@@ -28,42 +31,42 @@ else
 fi
 
 #-- Symlink classic "~/.foorc" dotfiles first
-( cd "$MYDIR"/HOME
+( cd "$MYDIR"/HOME || exit
 	for name in *
 	do
 		dest=~/.$name
 
 		#-- silently skip symlinks, probably we installed them already
-		[ ! -L $dest ] || continue
+		[ ! -L "$dest" ] || continue
 
-		if [ -d $dest ]
+		if [ -d "$dest" ]
 		then
 			echo "$dest is a plain directory; would not symlink"
 			continue
 		fi
 
-		echo -ne "installing rc-style... \t"
+		printf "installing rc-style... \t"
 		symlink "$PWD/$name" "$dest"
 	done
 )
 
 #-- Symlink XDG-style "~/.config/foo" as well
 STD_CFG=${XDG_CONFIG_HOME:-$HOME/.config}
-( cd "$MYDIR"/XDG
+( cd "$MYDIR"/XDG || exit
 	for name in *
 	do
 		dest="$STD_CFG"/$name
 
 		#-- silently skip symlinks, probably we installed them already
-		[ ! -L $dest ] || continue
+		[ ! -L "$dest" ] || continue
 
-		if test -d $dest
+		if test -d "$dest"
 		then
 			echo "Skipping pre-existing XDG dir: $dest"
 			continue
 		fi
 
-		echo -ne "installing XDG subdir... \t"
+		printf "installing XDG subdir... \t"
 		symlink "$PWD/$name" "$dest"
 	done
 )
